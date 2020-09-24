d33710169bfeae5535428a77ecb0fd505f3373dc
GriffinN14 <71406855+GriffinN14@users.noreply.github.com>
2020-09-24 09:27:48 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -11,3 +11,9 @@
 # volume
 #
 # test output radius of 3 should give volume of 84.8230016469
+import math
+R = input("what is the radius?")
+p = int(R)
+A = math.pi * pow(p,3)
+V = str(A)
+print ("The volume of the sphere is " + V)
